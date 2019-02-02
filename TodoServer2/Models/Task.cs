b56d82e88e505bda00f351c4852d73b0cb8c4008
Toomas Luts <toomasluts1@gmail.com>
2019-02-02 14:18:24 +0200

    Commit2

---
+++ TodoServer2/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoServer2.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string OwnerId { get; set; }
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public Boolean Marked_as_done { get; set; } = false;
+        public DateTime Created_at { get; set; } = DateTime.Now;
+    }
+}
